964fd176cd77acc2ec7181dcfa4a3dc2017ef3aa
Robert Sehr <robert.sehr@intranda.com>
2020-07-21 16:27:33 +0200

    create excel file for each image, export images

--- goobi-plugin-workflow-projectexport/src/main/java/de/intranda/goobi/plugins/ProjectExportPlugin.java
+++ goobi-plugin-workflow-projectexport/src/main/java/de/intranda/goobi/plugins/ProjectExportPlugin.java
@@ -1,25 +1,50 @@
 package de.intranda.goobi.plugins;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.goobi.beans.Process;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.interfaces.IWorkflowPlugin;
 
 import de.sub.goobi.config.ConfigPlugins;
 import de.sub.goobi.helper.Helper;
+import de.sub.goobi.helper.exceptions.DAOException;
+import de.sub.goobi.helper.exceptions.SwapException;
 import de.sub.goobi.persistence.managers.MySQLHelper;
+import de.sub.goobi.persistence.managers.ProcessManager;
 import de.sub.goobi.persistence.managers.ProjectManager;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
+import ugh.dl.DigitalDocument;
+import ugh.dl.DocStruct;
+import ugh.dl.Fileformat;
+import ugh.dl.Metadata;
+import ugh.exceptions.PreferencesException;
+import ugh.exceptions.ReadException;
+import ugh.exceptions.WriteException;
 
 @PluginImplementation
 @Log4j2
@@ -58,6 +83,15 @@ public class ProjectExportPlugin implements IWorkflowPlugin {
     @Getter
     private String projectValidationError = null;
 
+    @Setter
+    private String exportFolder;
+
+    // used for tests
+    @Setter
+    private boolean testDatabase;
+    @Setter
+    private List<Process> testList;
+
     public List<String> getAllProjectNames() {
         if (allProjectNames == null) {
             allProjectNames = ProjectManager.getAllProjectTitles(true);
@@ -70,51 +104,53 @@ public class ProjectExportPlugin implements IWorkflowPlugin {
             this.projectName = selectedProjectName;
             readConfiguration(projectName);
 
-            // check if all processes of the project have reached the configured task:
-            StringBuilder query = new StringBuilder();
-            query.append("SELECT COUNT(*) ");
-            query.append("FROM prozesse WHERE ");
-            query.append("projekteId = (SELECT projekteID FROM projekte WHERE titel = ?) ");
-            query.append("AND prozesseID NOT IN ( ");
-            query.append("SELECT prozesseId FROM schritte WHERE titel = ? AND Bearbeitungsstatus = 3) ");
-
             int numberOfTasks = getNumberOfUnfinishedTasks();
             if (numberOfTasks == 0) {
                 exportAllowed = true;
-                projectValidationError= null;
+                projectValidationError = null;
             } else {
                 exportAllowed = false;
-                projectValidationError= "The project '" + projectName + "' has " + numberOfTasks +" unfinished processes";
+                projectValidationError = "The project '" + projectName + "' has " + numberOfTasks + " unfinished processes";
                 Helper.setFehlerMeldung("project", projectValidationError, projectValidationError);
             }
         }
     }
 
     private int getNumberOfUnfinishedTasks() {
-        Connection connection = null;
-        StringBuilder query = new StringBuilder();
-        query.append("SELECT COUNT(*) ");
-        query.append("FROM prozesse WHERE ");
-        query.append("projekteId = (SELECT projekteID FROM projekte WHERE titel = ?) ");
-        query.append("AND prozesseID NOT IN ( ");
-        query.append("SELECT prozesseId FROM schritte WHERE titel = ? AND Bearbeitungsstatus = 3) ");
-        try {
-            connection = MySQLHelper.getInstance().getConnection();
-            int currentValue =
-                    new QueryRunner().query(connection, query.toString(), MySQLHelper.resultSetToIntegerHandler, projectName, finishStepName);
-            return currentValue;
-        } catch (SQLException e) {
-            log.error(e);
-        } finally {
-            if (connection != null) {
-                try {
-                    MySQLHelper.closeConnection(connection);
-                } catch (SQLException e) {
-                    log.error(e);
+        if (!testDatabase) {
+            Connection connection = null;
+            StringBuilder query = new StringBuilder();
+            query.append("SELECT COUNT(*) ");
+            query.append("FROM prozesse WHERE ");
+            query.append("projekteId = (SELECT projekteID FROM projekte WHERE titel = ?) ");
+            query.append("AND prozesseID NOT IN ( ");
+            query.append("SELECT prozesseId FROM schritte WHERE titel = ? AND Bearbeitungsstatus = 3) ");
+            try {
+                connection = MySQLHelper.getInstance().getConnection();
+                int currentValue =
+                        new QueryRunner().query(connection, query.toString(), MySQLHelper.resultSetToIntegerHandler, projectName, finishStepName);
+                return currentValue;
+            } catch (SQLException e) {
+                log.error(e);
+            } finally {
+                if (connection != null) {
+                    try {
+                        MySQLHelper.closeConnection(connection);
+                    } catch (SQLException e) {
+                        log.error(e);
+                    }
                 }
             }
         }
         return 0;
+    }
+
+    private List<Process> getProcessList() {
+        if (!testDatabase) {
+            return ProcessManager.getProcesses("prozesse.titel", "projekte.titel = '" + projectName + "' ");
+        } else {
+            return testList;
+        }
     }
 
     private void readConfiguration(String projectName) {
@@ -140,9 +176,130 @@ public class ProjectExportPlugin implements IWorkflowPlugin {
         finishStepName = config.getString("/finishedStepName");
     }
 
-
     public void prepareExport() {
 
+        List<Process> processesInProject = getProcessList();
+
+        // create excel file
+        Workbook wb = new XSSFWorkbook();
+        Sheet sheet = wb.createSheet("images");
+
+        // create header
+        Row headerRow = sheet.createRow(0);
+        headerRow.createCell(0).setCellValue("Image");
+        headerRow.createCell(1).setCellValue("Title");
+        headerRow.createCell(2).setCellValue("Identifier");
+        headerRow.createCell(3).setCellValue("physical page number");
+        headerRow.createCell(4).setCellValue("logical page number");
+
+        int rowCounter = 1;
+
+        for (Process process : processesInProject) {
+
+            // open mets file
+            try {
+                Fileformat fileformat = process.readMetadataFile();
+
+                DigitalDocument digDoc = fileformat.getDigitalDocument();
+
+                DocStruct anchor = null;
+                DocStruct logical = digDoc.getLogicalDocStruct();
+                if (logical.getType().isAnchor()) {
+                    anchor = logical;
+                    logical = logical.getAllChildren().get(0);
+                }
+                DocStruct physical = digDoc.getPhysicalDocStruct();
+
+                // create row for each image
+                if (physical.getAllChildren() != null) {
+                    String title = "";
+                    String identifier = "";
+                    for (Metadata md : logical.getAllMetadata()) {
+                        if (md.getType().getName().equals("TitleDocMain")) {
+                            title = md.getValue();
+                        } else if (md.getType().getName().equals("CatalogIDDigital")) {
+                            identifier = md.getValue();
+                        }
+                    }
+                    for (DocStruct page : physical.getAllChildren()) {
+
+                        String physPageNo = "";
+                        String logPageNo = "";
+                        for (Metadata md : page.getAllMetadata()) {
+                            if (md.getType().getName().equals("physPageNumber")) {
+                                physPageNo = md.getValue();
+                            } else if (md.getType().getName().equals("logicalPageNumber")) {
+                                logPageNo = md.getValue();
+                            }
+                        }
+                        Row imageRow = sheet.createRow(rowCounter);
+                        imageRow.createCell(0).setCellValue(exportFolder + identifier + "/" + page.getImageName());
+                        imageRow.createCell(1).setCellValue(title);
+                        imageRow.createCell(2).setCellValue(identifier);
+                        imageRow.createCell(3).setCellValue(physPageNo);
+                        imageRow.createCell(4).setCellValue(logPageNo);
+                        rowCounter = rowCounter + 1;
+                    }
+                    // export images
+                    // TODO
+                    Path source = Paths.get(process.getImagesTifDirectory(false));
+                    Path target = Paths.get(exportFolder, identifier);
+                    if (!Files.exists(target)) {
+                        Files.createDirectories(target);
+                    }
+                    FileUtils.copyDirectory(source.toFile(), target.toFile());
+                    //                    StorageProvider.getInstance().copyDirectory(Paths.get(process.getImagesTifDirectory(false)), Paths.get(exportFolder + identifier));
+                }
+
+            } catch (ReadException | PreferencesException | WriteException | IOException | InterruptedException | SwapException | DAOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+        // save/download excel
+        try {
+            OutputStream out = Files.newOutputStream(Paths.get(exportFolder, "metadata.xlsx"));
+            wb.write(out);
+            out.flush();
+            out.close();
+            wb.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        for (Process process : processesInProject) {
+
+            // TODO close step xy via goobiscript?
+
+        }
     }
+
+    public static ResultSetHandler<Map<Integer, Map<String, String>>> resultSetToProjectMetadata =
+            new ResultSetHandler<Map<Integer, Map<String, String>>>() {
+        @Override
+        public Map<Integer, Map<String, String>> handle(ResultSet rs) throws SQLException {
+            Map<Integer, Map<String, String>> projectMetadata = new HashMap<>();
+            try {
+                while (rs.next()) {
+                    Integer id = rs.getInt("processid");
+                    String name = rs.getString("name");
+                    String value = rs.getString("value");
+
+                    Map<String, String> map = projectMetadata.get(id);
+                    if (map == null) {
+                        map = new HashMap<>();
+                        projectMetadata.put(id, map);
+                    }
+
+                }
+            } finally {
+                if (rs != null) {
+                    rs.close();
+                }
+            }
+            return projectMetadata;
+        }
+    };
 
 }
--- goobi-plugin-workflow-projectexport/src/test/java/de/intranda/goobi/plugins/ProjectExportPluginTest.java
+++ goobi-plugin-workflow-projectexport/src/test/java/de/intranda/goobi/plugins/ProjectExportPluginTest.java
@@ -2,14 +2,21 @@ package de.intranda.goobi.plugins;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.apache.commons.dbutils.QueryRunner;
 import org.easymock.EasyMock;
 import org.goobi.beans.Process;
 import org.goobi.beans.Project;
@@ -27,13 +34,19 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import de.sub.goobi.config.ConfigPlugins;
 import de.sub.goobi.config.ConfigurationHelper;
+import de.sub.goobi.helper.VariableReplacer;
 import de.sub.goobi.helper.enums.StepStatus;
+import de.sub.goobi.metadaten.MetadatenHelper;
+import de.sub.goobi.persistence.managers.MySQLHelper;
 import de.sub.goobi.persistence.managers.ProcessManager;
 import de.sub.goobi.persistence.managers.PropertyManager;
 import de.sub.goobi.persistence.managers.StepManager;
+import ugh.dl.Fileformat;
+import ugh.dl.Prefs;
+import ugh.fileformats.mets.MetsMods;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ ConfigPlugins.class, StepManager.class, ConfigurationHelper.class, ProcessManager.class, PropertyManager.class })
+@PrepareForTest({MetadatenHelper.class,VariableReplacer.class, ConfigPlugins.class, StepManager.class, ConfigurationHelper.class, ProcessManager.class, PropertyManager.class, MySQLHelper.class, QueryRunner.class})
 @PowerMockIgnore({ "javax.management.*" })
 public class ProjectExportPluginTest {
 
@@ -42,6 +55,7 @@ public class ProjectExportPluginTest {
     private File processDirectory;
     private File metadataDirectory;
     private Process process;
+    private String processMediaFolder;
 
     @Before
     public void setUp() throws Exception {
@@ -50,10 +64,20 @@ public class ProjectExportPluginTest {
         processDirectory.mkdirs();
         String metadataDirectoryName = metadataDirectory.getAbsolutePath() + File.separator;
 
+        // copy meta.xml
+        Path metaSource = Paths.get("src/test/resources/meta.xml");
+        Path metaTarget = Paths.get(processDirectory.getAbsolutePath(), "meta.xml");
+        Files.copy(metaSource, metaTarget);
+
+
+        // copy ruleset
+
         XMLConfiguration config = getConfig();
         PowerMock.mockStatic(ConfigPlugins.class);
         EasyMock.expect(ConfigPlugins.getPluginConfig("intranda_workflow_projectexport")).andReturn(config).anyTimes();
-        PowerMock.replay(ConfigPlugins.class);
+
+
+
 
         PowerMock.mockStatic(ConfigurationHelper.class);
         ConfigurationHelper configurationHelper = EasyMock.createMock(ConfigurationHelper.class);
@@ -64,65 +88,59 @@ public class ProjectExportPluginTest {
         EasyMock.expect(configurationHelper.isUseMasterDirectory()).andReturn(true).anyTimes();
         EasyMock.expect(configurationHelper.isCreateMasterDirectory()).andReturn(false).anyTimes();
         EasyMock.expect(configurationHelper.isCreateSourceFolder()).andReturn(false).anyTimes();
-        EasyMock.expect(configurationHelper.getMediaDirectorySuffix()).andReturn("media").anyTimes();
-        EasyMock.expect(configurationHelper.getMasterDirectoryPrefix()).andReturn("master").anyTimes();
+        EasyMock.expect(configurationHelper.getProcessImagesMainDirectoryName()).andReturn("fixture_media").anyTimes();
         EasyMock.expect(configurationHelper.getFolderForInternalProcesslogFiles()).andReturn("intern").anyTimes();
         EasyMock.expect(configurationHelper.getMetadataFolder()).andReturn(metadataDirectoryName).anyTimes();
 
         EasyMock.expect(configurationHelper.getScriptCreateDirMeta()).andReturn("").anyTimes();
         EasyMock.replay(configurationHelper);
+        PowerMock.mockStatic(VariableReplacer.class);
+        EasyMock.expect(VariableReplacer.simpleReplace(EasyMock.anyString(), EasyMock.anyObject())).andReturn("fixture_media");
+        PowerMock.replay(VariableReplacer.class);
+        //        PowerMock.mockStatic( MySQLHelper.class);
+
+        //        Connection connection = EasyMock.createNiceMock(Connection.class);
+        //        MySQLHelper mysqlHelper = EasyMock.createMock(MySQLHelper.class);
+        //        EasyMock.replay(connection);
+        //
+        //        EasyMock.expect(MySQLHelper.getInstance()).andReturn(mysqlHelper).anyTimes();
+        //        EasyMock.expect(mysqlHelper.getConnection()).andReturn(connection).anyTimes();
+        //        mysqlHelper.closeConnection(connection);
+        //
+        //        QueryRunner runner = EasyMock.createMock(QueryRunner.class);
+        //        PowerMock.expectNew(QueryRunner.class).andReturn(runner).anyTimes();
+        //        List<Integer> ids = new ArrayList<>();
+        //        ids.add(1);
+        //        EasyMock.expect(runner.query(connection, EasyMock.anyString(), MySQLHelper.resultSetToIntegerHandler,EasyMock.anyString(), EasyMock.anyString())).andReturn(0).anyTimes();
+
+        //        EasyMock.expect(runner.query(connection, EasyMock.anyString(), MySQLHelper.resultSetToIntegerListHandler,"SampleProject")).andReturn(ids).anyTimes();
+
+        Prefs prefs = new Prefs();
+        prefs.loadPrefs("src/test/resources/ruleset.xml");
+        Fileformat ff = new MetsMods(prefs);
+        ff.read(metaTarget.toString());
+
+        PowerMock.mockStatic(MetadatenHelper.class);
+        EasyMock.expect(MetadatenHelper.getMetaFileType(EasyMock.anyString())).andReturn("mets");
+        EasyMock.expect(MetadatenHelper.getFileformatByName(EasyMock.anyString(), EasyMock.anyObject())).andReturn(ff);
 
         PowerMock.mockStatic(StepManager.class);
         PowerMock.mockStatic(ProcessManager.class);
-        PowerMock.mockStatic(PropertyManager.class);
         PowerMock.replay(ConfigPlugins.class);
         PowerMock.replay(ConfigurationHelper.class);
-
+        PowerMock.replay(MetadatenHelper.class);
         process = getProcess();
+        EasyMock.expect(ProcessManager.getProcessById(EasyMock.anyInt())).andReturn(process).anyTimes();
+        PowerMock.replay(ProcessManager.class);
+        PowerMock.mockStatic(PropertyManager.class);
+        //        EasyMock.replay(runner);
+        //        PowerMock.replay(mysqlHelper);
+
+        //        PowerMock.replay(MySQLHelper.class);
+
 
     }
 
-    private void createProcessDirectory(boolean createSourceDirectory, boolean createThumbsDirectory, boolean createOcrDirectory) throws IOException {
-
-        // image folder
-        File imageDirectory = new File(processDirectory.getAbsolutePath(), "images");
-        imageDirectory.mkdir();
-        // master folder
-        File masterDirectory = new File(imageDirectory.getAbsolutePath(), "master_fixture_media");
-        masterDirectory.mkdir();
-        File masterImageFile = new File(masterDirectory.getAbsolutePath(), "0001.tif");
-        masterImageFile.createNewFile();
-        // media folder
-        File mediaDirectory = new File(imageDirectory.getAbsolutePath(), "fixture_media");
-        mediaDirectory.mkdir();
-        File mediaImageFile = new File(mediaDirectory.getAbsolutePath(), "0001.tif");
-        mediaImageFile.createNewFile();
-        if (createSourceDirectory) {
-            // source folder
-            File sourceDirectory = new File(imageDirectory.getAbsolutePath(), "fixture_source");
-            sourceDirectory.mkdir();
-            File sourceFile = new File(sourceDirectory.getAbsolutePath(), "fixture.zip");
-            sourceFile.createNewFile();
-        }
-        // thumbs
-        if (createThumbsDirectory) {
-            File thumbsDirectory = new File(processDirectory.getAbsolutePath(), "thumbs");
-            thumbsDirectory.mkdir();
-            File thumbsMediaDirectory = new File(thumbsDirectory.getAbsolutePath(), "fixture_media_800");
-            thumbsMediaDirectory.mkdir();
-            File thumbsImageFile = new File(thumbsMediaDirectory.getAbsolutePath(), "0001.tif");
-            thumbsImageFile.createNewFile();
-        }
-        // ocr
-        if (createOcrDirectory) {
-            File ocr = new File(processDirectory.getAbsolutePath(), "ocr");
-            ocr.mkdir();
-            File altoDirectory = new File(ocr.getAbsolutePath(), "fixture_alto");
-            altoDirectory.mkdir();
-            File altoFile = new File(altoDirectory.getAbsolutePath(), "0001.xml");
-            altoFile.createNewFile();
-        }
-    }
 
     @Test
     public void testConstructor() {
@@ -133,12 +151,46 @@ public class ProjectExportPluginTest {
     @Test
     public void testConfigurationFile() {
         ProjectExportPlugin plugin = new ProjectExportPlugin();
+        plugin.setTestDatabase(true);
         plugin.setProjectName("SampleProject");
 
         assertEquals("Export with PREMIS data", plugin.getFinishStepName());
 
     }
 
+    @Test
+    public void testPrepareExport() {
+        ProjectExportPlugin plugin = new ProjectExportPlugin();
+        plugin.setTestDatabase(true);
+        plugin.setExportFolder(metadataDirectory.getAbsolutePath());
+        List<Process> testProcesses = new ArrayList<>();
+        testProcesses.add(process);
+        plugin.setTestList(testProcesses);
+
+        plugin.setProjectName("SampleProject");
+
+        plugin.prepareExport();
+
+        Path excelFile = Paths.get(metadataDirectory.getAbsolutePath(), "metadata.xlsx");
+        assertTrue(Files.exists(excelFile));
+        // TODO read content of file
+
+        Path imageFolder =  Paths.get(metadataDirectory.getAbsolutePath(), "517154005");
+        assertTrue(Files.exists(imageFolder));
+
+        List<String> fileNames = new ArrayList<>();
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(imageFolder)) {
+            for (Path path : directoryStream) {
+                if (!path.getFileName().toString().startsWith(".")) {
+                    fileNames.add(path.getFileName().toString());
+                }
+            }
+        } catch (IOException ex) {
+        }
+        Collections.sort(fileNames);
+        assertEquals("00000001.tif", fileNames.get(0));
+
+    }
 
     public Process getProcess() {
         Project project = new Project();
@@ -177,7 +229,12 @@ public class ProjectExportPluginTest {
         steps.add(s3);
 
         process.setSchritte(steps);
-
+        try {
+            createProcessDirectory();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return process;
     }
 
@@ -187,5 +244,40 @@ public class ProjectExportPluginTest {
         config.setReloadingStrategy(new FileChangedReloadingStrategy());
         return config;
 
+    }
+
+
+
+    private void createProcessDirectory() throws IOException {
+
+        // image folder
+        File imageDirectory = new File(processDirectory.getAbsolutePath(), "images");
+        imageDirectory.mkdir();
+        // master folder
+        File masterDirectory = new File(imageDirectory.getAbsolutePath(), "master_fixture_media");
+        masterDirectory.mkdir();
+        for (int i =1; i <= 16; i++) {
+            createFile(masterDirectory, i);
+        }
+
+        // media folder
+        File mediaDirectory = new File(imageDirectory.getAbsolutePath(), "fixture_media");
+        mediaDirectory.mkdir();
+        for (int i =1; i <= 16; i++) {
+            createFile(mediaDirectory, i);
+        }
+        processMediaFolder= mediaDirectory.getAbsolutePath();
+
+    }
+
+    private void createFile(File folder, int counter) throws IOException {
+        String imagename;
+        if (counter > 9) {
+            imagename = "000000" + counter + ".tif";
+        } else {
+            imagename = "0000000" + counter + ".tif";
+        }
+        File image = new File(folder.getAbsolutePath(), imagename);
+        image.createNewFile();
     }
 }
